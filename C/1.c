f155b51e43658c8ddd238686948217817bc9c510
QXQZX <501966782@qq.com>
2018-10-18 22:57:29 +0800

    2018-10-18-22:57

--- C/1.c
+++ C/1.c
@@ -1,23 +1,41 @@
 #include <stdio.h>
-    int
-    main()
+#include <string.h>
+#include <stdlib.h>
+union node
 {
-    int t1, t2, t3, h1, h2, h3, m1, m2, m3, s1, s2, s3;
-    scanf("%2d:%2d:%2d", &h1, &m1, &s1);
-    scanf("%2d:%2d:%2d", &h2, &m2, &s2);
-    t1 = h1 * 3600 + m1 * 60 + s1;
-    t2 = h2 * 3600 + m2 * 60 + s2;
-    if (t1 > t2)
-    {
-        t3 = t1 - t2;
-    }
-    else
-    {
-        t3 = t2 - t1;
-    }
-    h3 = t3 / 3600;
-    m3 = t3 % 3600 / 60;
-    s3 = t3 % 3600 % 60;
-    printf("%02d:%02d:%02d", h3, m3, s3);
-    return 0;
+    int a;
+    double b;
+    char s[30];
+}q[112345];
+char book[112345][30];
+int main()
+{
+    int n, m;
+    scanf("%d %d", &n, &m);
+    int i;
+    for(i = 0;i < n;i++){
+        scanf("%s", book[i]);
+        if(strcmp(book[i],"INT") == 0){
+            scanf("%d", &q[i].a);
+        }else if(strcmp(book[i], "DOUBLE") == 0){
+            scanf("%lf", &q[i].b);
+        }else if(strcmp(book[i],"STRING") == 0){
+            scanf("%s", q[i].s);
+        }
+    }
+    int k;
+    for(i = 0;i < m;i++){
+        scanf("%d", &k);
+        if(strcmp(book[k],"INT")  == 0){
+            printf("%d\n", q[k].a);
+        }else if(strcmp(book[k], "DOUBLE") == 0){
+            printf("%.2f\n", q[k].b);
+        }else if(strcmp(book[k],"STRING") == 0){
+            printf("%s\n", q[k].s);
+        }
+    }
+
+
+    return 0;
 }
+
